626b71245e7578ffee1295aedf57c1fada43e0c8
Fílip Anselmo <filipanselmoalvesdasilva870@gmail.com>
2024-06-05 11:40:01 -0400

    feat: add endpoint de login, já com o token

--- app/auth_user.py
+++ app/auth_user.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from fastapi import status
 from fastapi.exceptions import HTTPException
 from sqlalchemy.orm import Session
@@ -5,7 +6,12 @@ from sqlalchemy.exc import IntegrityError
 from app.models.user import UserModel
 from app.schemas.user import UserSchema
 from passlib.context import CryptContext
+from jose import jwt, JWTError
+from decouple import config
 
+
+SECRET_KEY=config('SECRET_KEY')
+ALGORITHM=config('ALGORITHM')
 crypt_context = CryptContext(schemes=['sha256_crypt'])
 
 class UserUseCases:
@@ -22,3 +28,31 @@ class UserUseCases:
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail='Usuário já existe'
             )
+        
+    def user_login(self,user:UserSchema, expires_in:int = 30):
+        user_on_db = self.db_session.query(UserModel).filter_by(username=user.username).first()
+
+        if user_on_db is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Nome de usuário ou senha inválidos'
+            )
+        
+        if not crypt_context.verify(user.password, user_on_db.password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail='Nome de usuário ou senha inválidos'
+            )
+        
+        exp = datetime.utcnow() + timedelta(minutes=expires_in)
+        data = {
+            'sub': user.username,
+            'exp': exp
+        }
+
+        access_token = jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
+
+        return {
+            'access_token': access_token,
+            'exp': exp.isoformat()
+        }
--- app/routers/users.py
+++ app/routers/users.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends, status
 from fastapi.responses import JSONResponse
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from app.dependencies import get_db_sesion
 from app.auth_user import UserUseCases
@@ -13,5 +14,15 @@ async def user_register(user:UserSchema,db_session:Session=Depends(get_db_sesion
     uc.user_register(user=user)
     return JSONResponse(
         content={'msg': 'Success'},
+        status_code=status.HTTP_201_CREATED
+    )
+
+@router.post('/login')
+async def user_login(request_form_user: OAuth2PasswordRequestForm=Depends(), db_session:Session=Depends(get_db_sesion)):
+    uc = UserUseCases(db_session=db_session)
+    user = UserSchema(username=request_form_user.username, password=request_form_user.password)
+    auth_data = uc.user_login(user=user)
+    return JSONResponse(
+        content=auth_data,
         status_code=status.HTTP_201_CREATED
     )
